71030914149fa1fd456b1bbd914291ae477b08be
FrankZTH <frankboygx@gmail.com>
2017-06-21 08:37:56 +0800

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,43 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-cssvars'),
-nested = require('postcss-nested');
+let gulp = require('gulp'),
+  watch = require('gulp-watch'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+
+  uglify = require('gulp-uglify'),
+  rename = require("gulp-rename"),
+  runSequence = require('run-sequence'),
+  babel = require('gulp-babel'),//es6 -> 原生
+  plumber = require('gulp-plumber'),
+  sourcemaps = require('gulp-sourcemaps');
 
 
-gulp.task('default', function(){
-  console.log('Hello GULP');
+gulp.task('css', () => {
+  gulp.src('./css/*.css')
+    .pipe(plumber())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./newCss'))
 });
 
-gulp.task('html', function(){
-  console.log('HTML task ...');
+gulp.task('jsUglify', () => {
+  gulp.src(['./js/donate.js'])
+    .pipe(plumber())
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./min.js'))
+})
+
+gulp.task('watch', () => {
+  watch('js/donate.js', ['jsUglify']);
+  watch('css/*.css', ['css']);
 });
 
-gulp.task('css', function(){
-  console.log('CSS task ...');
+gulp.task('writeConfig', () => {
+  // 寫你的設定黨啦小廢物
 });
 
-gulp.task('watch', function(){
-  watch('index.html', function(){
-    gulp.start('html');
-  });
-  watch('css/style.css', function(){
-    gulp.start('css');
-  });
-});
+gulp.task('default', () => runSequence('writeConfig', ['jsUglify', 'watch']));
